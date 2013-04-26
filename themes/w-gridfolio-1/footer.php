ab98cdb8f4213ff206e3698527313ce4a8b86c5f
RYO NAGAI <seinaoasis@gmail.com>
2013-04-26 18:57:51 +0900

    コピーwright消す

--- themes/w-gridfolio-1/footer.php
+++ themes/w-gridfolio-1/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer text is in the Header.php file -->
-<div id="footerbox">&copy; <?php the_date('Y'); ?> <?php bloginfo('name'); ?><br /><a href="http://www.w-portfolio.com">Built on W&mdash;Portfolio</a></div><!-- footer/copyright area -->
+<div id="footerbox">&copy; <?php the_date('Y'); ?> <?php bloginfo('name'); ?><br /><a href="http://www.w-portfolio.com"></a></div><!-- footer/copyright area -->
 </div><!-- main div -->
 </div><!-- wrapper div -->
 </div>
