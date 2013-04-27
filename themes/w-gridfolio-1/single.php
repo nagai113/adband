ead5c34a74aee5a0cb47c8940a0f3cc65797d4af
nagai113 <nagai@mousousakusen.com>
2013-04-27 14:21:02 +0900

    サイドにあった過去コンテンツのサムネイル削除

--- themes/w-gridfolio-1/single.php
+++ themes/w-gridfolio-1/single.php
@@ -36,42 +36,8 @@
 <?php my_attachment_gallery(0, 'large', 'alt="' . $post->post_title . '"'); ?>
 </div>
 -->
-<div id="relatedposts">
-<?php $orig_post = $post;
-global $post;
-$categories = get_the_category($post->ID);
-if ($categories) {
-$category_ids = array();
-foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
-$args=array(
-'category__in' => $category_ids,
-'post__not_in' => array($post->ID),
-'posts_per_page'=> 4, // Number of related posts that will be shown.
-'caller_get_posts'=>1
-);
-$my_query = new wp_query( $args );
-if( $my_query->have_posts() ) {
-echo '<span class="pagetitle"><strong>Related:</strong></span><br /><br />';
-while( $my_query->have_posts() ) {
-$my_query->the_post();?>
 
-<figure>
-<figcaption class="relatedcap">
-<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-</figcaption>
-<div id="relatedthumb">
-<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
-</div>
-</figure><br />
 
-<?
-}
-echo '';
-}
-}
-$post = $orig_post;
-wp_reset_query(); ?>
-</div>
 
 <?php endwhile; // end of the loop. ?>
 
